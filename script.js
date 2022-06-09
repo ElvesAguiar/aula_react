58418e7504a040ffefaaf3aff5be657c63391ccc
Elves Aguiar <elvesmhs@hotmail.com>
2022-06-09 16:47:58 -0300

    fist commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var lista = ['matheus','elves','vitorian','karol'];
+
+lista2=[1,2,3,4,5];
+lista.join(li)
+
+
+function mostrar(){
+    Text=document.getElementById(area);
+    lista.forEach(element => {
+        document.write(element+" "); 
+        document.write("<br> </br>")   
+    });
+    
+}
