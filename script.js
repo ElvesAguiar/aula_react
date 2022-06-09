79355175f0afdef72e91e6a5d572db33b4172b1d
Elves Aguiar <elvesmhs@hotmail.com>
2022-06-09 17:03:30 -0300

    aprendendo Lacos de repetição

--- script.js
+++ script.js
@@ -10,5 +10,31 @@ function mostrar(){
         document.write(element+" "); 
         document.write("<br> </br>")   
     });
-    
+    let a = 0;
+    while (a <10){
+        console.log("a= "+a);
+        a++;
+    }
+   
+    console.log("----------------------------------");
+    for (a=0;a<10;a++){
+        console.log("b= "+a);
+      
+    }
+    let b=2;
+    switch(b){
+        case 1:
+        console.log("Caso 1");
+        break;
+        case 2:
+        console.log("Caso 2");
+        break;
+        case 3:
+        console.log("Caso 3");
+        break;
+        default:
+        console.log("Caso nenhum");
+        break;    
+    }
+
 }
